c5efc7f3af75e42e0137fafaf641a95aed1240a6
Your Name <you@example.com>
2025-07-02 17:05:45 -0500

    Tarea En Clases

--- src/utils/ContactoComparator.java
+++ src/utils/ContactoComparator.java
@@ -2,12 +2,20 @@ package utils;
 
 import java.util.Comparator;
 
+import models.Contacto;
+
 public class ContactoComparator implements Comparator<Contacto> {
 
     @Override
     public int compare(Contacto o1, Contacto o2) {
-        // TODO Auto-generated method stub
-        return 0;
+        
+        int resultComparationApellido = o1.getApellido().compareToIgnoreCase(o2.getApellido());
+
+        if(resultComparationApellido !=0){
+            return resultComparationApellido;
+        }
+        int resultComparationNombre = o1.getNombre().compareToIgnoreCase(o2.getNombre());
+        return resultComparationNombre;
     }
 
     
